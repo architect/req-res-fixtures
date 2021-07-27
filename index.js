8d5390ad6a086ac55d1f76ec969297ad3e57ce57
Ryan Block <rblock@gmail.com>
2021-07-26 17:19:25 -0700

    Add legacy fixtures

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /* eslint-disable global-require */
 module.exports = {
   http: {
-    req: require('./fixtures/http-req'),
-    res: require('./fixtures/http-res'),
+    req: require('./fixtures/http/req'),
+    res: require('./fixtures/http/res'),
+    legacy: {
+      req: require('./fixtures/http/legacy/req'),
+      res: require('./fixtures/http/legacy/res'),
+    }
   },
   ws: {
-    req: require('./fixtures/ws-req'),
+    req: require('./fixtures/ws/req'),
   }
 }
